b251292ac02061cd42d5a594a4a74546bf0cba1f
Ryoichi MATSUMOTO <iyo.rotomus@gmail.com>
2024-12-14 08:48:13 +0900

    tentative (for TVM BYOC for Arm Compute Library)

--- models/pytorch/pytorch_model_base.py
+++ models/pytorch/pytorch_model_base.py
@@ -54,3 +54,46 @@ class PyTorchModelBase():
         lib_path = Path(output_dir, 'model.so')
         cross_compile = 'aarch64-linux-gnu-gcc'
         lib.export_library(lib_path, cc=cross_compile)
+
+    def convert_to_arm_via_tvm_byoc(self, output_dir='arm_byoc', input_name='modelInput', output_name='modelOutput') -> None:
+        '''
+        Convert the PyTorch model to Arm Compute Library format via TVM using the BYOC (Bring Your Own Codegen) approach
+        '''
+        # --- Create the output directory if it does not exist ---
+        Path(output_dir).mkdir(parents=True, exist_ok=True)
+        
+        # --- Convert PyTorch model to TVM Relay ---
+        input_shape = self.input_size
+        input_name = 'modelInput'
+        input_dtype = 'float32'
+        shape_list = [(input_name, input_shape)]
+        
+        # --- Ensure the model is in evaluation mode ---
+        self.net.eval()
+        # --- Ensure the model is on the CPU ---
+        self.net.cpu()
+
+        # --- Trace the model with a dummy input ---
+        dummy_input = torch.randn(input_shape)
+        traced_model = torch.jit.trace(self.net, dummy_input).eval()
+        self.net.to(self.device)
+        
+        # --- Convert the traced model to TVM Relay ---
+        mod, params = relay.frontend.from_pytorch(traced_model, shape_list)
+        
+        # --- Partition the model for Arm Compute Library ---
+        #  - https://discuss.tvm.apache.org/t/byoc-how-can-i-use-arm-compute-library-integration-flow-for-qnn/8882
+        #  - https://tvm.apache.org/docs/how_to/deploy/arm_compute_lib.html
+        from tvm.relay.op.contrib.arm_compute_lib import partition_for_arm_compute_lib
+        mod = partition_for_arm_compute_lib(mod)
+        print(mod.astext())
+        
+        # --- Compile Relay model with TVM ---
+        with tvm.transform.PassContext(opt_level=3):
+            target = 'llvm -mtriple=aarch64-linux-gnu -mattr=+neon'
+            lib = relay.build(mod, target=target, params=params)
+
+        # --- Export the compiled model to the Arm Compute Library format ---
+        lib_path = Path(output_dir, 'model.so')
+        cross_compile = 'aarch64-linux-gnu-gcc'
+        lib.export_library(lib_path, cc=cross_compile)
